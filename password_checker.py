6683525a9d6907fd85bf6113b9eff1e823afbcef
romaisarwp <romaisarwp@gmail.com>
2026-08-28 09:59:51 +0500

    Add uppercase letter validation

--- password_checker.py
+++ password_checker.py
@@ -1,9 +1,25 @@
 print("Password Strength Checker")
 password = input("Enter your password: ")
 print("You entered:", password)
+
+# Check password length
+
 length = len(password)
 print("Password length:", length)
 if length >= 8:
     print("Length check: Passed")
 else:
     print("Length check: Failed")
+
+# Check for uppercase letters
+
+has_uppercase = False
+
+for character in password:
+    if character.isupper():
+        has_uppercase = True
+
+if has_uppercase:
+    print("Uppercase check: Passed")
+else:
+    print("Uppercase check: Failed")
