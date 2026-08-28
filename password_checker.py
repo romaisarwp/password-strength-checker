7452f6ccc0b6136f587fe1969d2e8a379dd9915b
romaisarwp <romaisarwp@gmail.com>
2026-08-28 09:43:02 +0500

    Add password length validation

--- password_checker.py
+++ password_checker.py
@@ -1,3 +1,9 @@
 print("Password Strength Checker")
 password = input("Enter your password: ")
 print("You entered:", password)
+length = len(password)
+print("Password length:", length)
+if length >= 8:
+    print("Length check: Passed")
+else:
+    print("Length check: Failed")
